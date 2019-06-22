0c9493f73a388727618519e5b78ac4d593c16365
Dave-mash <macharia3041@gmail.com>
2019-06-23 00:33:21 +0300

    added car slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { CarsListComponent } from './components/cars-list/cars-list.component';
 import { CarFeaturesFormComponent } from './components/car-features-form/car-features-form.component';
+import { CarSliderComponent } from './components/car-slider/car-slider.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     CarsListComponent,
-    CarFeaturesFormComponent
+    CarFeaturesFormComponent,
+    CarSliderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

e62fa0d3915be27da46dca35172a6d7bb8a12da9
Dave-mash <macharia3041@gmail.com>
2019-06-23 11:06:52 +0300

    Added a car item

--- src/app/components/cars-list/cars-list.component.ts
+++ src/app/components/cars-list/cars-list.component.ts
@@ -6,6 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cars-list.component.css']
 })
 export class CarsListComponent implements OnInit {
+  pageTitle: string = 'Cars List';
+  cars: any[] = [
+    {
+      "make": "Toyota",
+      "model": "Hilux",
+      "color": "silver",
+      "condition": "new",
+      "price": "ksh.2,000,000",
+      "rating": 4.2
+    },
+    {
+      "make": "Nissan",
+      "model": "Sunny",
+      "color": "black",
+      "condition": "new",
+      "price": "ksh.1,500,000",
+      "rating": 3.5
+    },
+    {
+      "make": "Mazda",
+      "model": "Demio",
+      "color": "Blue",
+      "condition": "old",
+      "price": "ksh.2,200,000",
+      "rating": 4.0
+    }
+  ];
 
   constructor() { }
 
